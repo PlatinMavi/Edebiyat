3110e5a100c4980927f661d0f289cbbfa5b5bb6c
PattisHesap1 <128600900+PattisHesap1@users.noreply.github.com>
2023-04-06 11:31:57 +0300

    Update v1.py

--- api/v1.py
+++ api/v1.py
@@ -1,20 +1,36 @@
 from django.urls import path, include
 from django.http import HttpResponse, JsonResponse, HttpRequest
 from index.models import Kitap, Comment, Dergi, Siir, Yazar,filter_comment
-
+import time
 def endpoint_comment_post(request:HttpRequest, id : int):
     if request.method.lower() != "post":
         return HttpResponse("Method Not Allowed!")
     if id == None:
         return HttpResponse("")
-    is_hide_name = request.headers.get("is_hide_name")
-    is_spoilers = request.headers.get("is_spoilers")
+    is_hide_name = request.headers.get("is_hide_name").lower()
+    is_spoilers = request.headers.get("is_spoilers").lower()
     author_name = request.headers.get("author_name")
     message = request.headers.get("author_name")
     headers = request.headers.__dict__
+    if not (is_hide_name in ("true","false")):
+        is_hide_name = False
+    is_hide_name = bool(is_hide_name)
+    
+    if not (is_spoilers in ("true","false")):
+        is_spoilers = False
+    is_spoilers = bool(is_hide_name)
+    
+    if not message or len(message) < 20 or len(message) > 1000:
+        return error_response(f"message length should be between 20 and 1000")
+    if not author_name or len(author_name) < 30 or len(author_name) > 5:
+        return error_response(f"author name should be between 5 and 30")
 
     return JsonResponse(True,safe=False)
 
+def error_response(error_desc):
+    return JsonResponse(
+        {"success":False,"error":error_desc or "","time":time.time()}
+    )
 
 def endpoint_comment_list(request, id):
     return JsonResponse(
