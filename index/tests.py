c8d515ac365c91b7a65c192beba93fbed1c25c3d
phasenull <55253035+phasenull@users.noreply.github.com>
2023-04-14 21:50:52 +0300

    Fixed some keywords in index.html

--- index/tests.py
+++ index/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from index.models import Comment,Creator,LiteratureObject,Vote,VoteableObject
+
+class AnimalTestCase(TestCase):
+	def setUp(self):
+		self.assertTrue(True,"test haha")
+
+	def test_animals_can_speak(self):
+		"""Animals that can speak are correctly identified"""
+		self.assertTrue(True,"test2 haha")
--- index/views.py
+++ index/views.py
@@ -7,25 +7,22 @@ import datetime
 # Create your views here.
 def index(request):
 
-    dergi = LiteratureObject.objects.filter(type="magazine").all().first()
-    kitap = LiteratureObject.objects.filter(type="book").all().first()
-    siir = LiteratureObject.objects.filter(type="poem").all().first()
-    aykitap = LiteratureObject.objects.all().order_by("interest_rate").last()
+    magazines = LiteratureObject.objects.filter(type="magazine").all()
+    books = LiteratureObject.objects.filter(type="book").all()
+    top_poem = LiteratureObject.objects.filter(type="poem").all().first()
+    book_of_the_month = LiteratureObject.objects.all().order_by("interest_rate").last()
     zaman = datetime.datetime.now()
     zaman_str = zaman.strftime("%Y-%m-%d")
     aylar = ["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"]
-    buay = aylar[int(zaman.month)-1]
-    yazar = Creator.objects.all().filter(created_at=zaman)
-    yazar1 = yazar[:5]
-    yazar2 = yazar[5:10]
+    this_month = aylar[int(zaman.month)-1]
+    birthday_creator = Creator.objects.all().filter(created_at__month=zaman.month, type = "writer").order_by("created_at")
     template = loader.get_template("index.html")
     context={
-        "DERGI":dergi and dergi.filtered_json(),
-        "KITAP":kitap and kitap.filtered_json(),
-        "YAZAR1":yazar1,
-        "YAZAR2":yazar2,
-        "SIIR":siir and siir.filtered_json(),
-        "AY":buay,
-        "AYKITAP": aykitap and siir.filtered_json(),
+        "TOP_MAGAZINE": magazines and [magazine.filtered_json() for magazine in magazines],
+        "TOP_BOOK": books and [book.filtered_json() for book in books],
+        "CREATOR_BIRTHDAY": birthday_creator,
+        "TOP_POEM": top_poem and top_poem.filtered_json(),
+        "THIS_MONTH": this_month,
+        "BOOK_OF_THE_MONTH": book_of_the_month and top_poem.filtered_json(),
     }
     return HttpResponse(template.render(context, request))
